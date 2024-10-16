8d463dc77958e02a6cb886cf78d0c2fd29c6005d
JaeLam <494595074@qq.com>
2024-10-17 00:13:36 +0800

    chore: 去除冗余

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,3 @@ export * from './colors'
 export * from './plugin'
 export * from './types'
 export * from './default-layout'
-
-export { tv } from './utils/tv'
-export type { VariantProps, TV } from 'tailwind-variants'
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,7 +8,6 @@ export {
   absoluteFullClasses,
   collapseAdjacentVariantBorders,
 } from './classes'
-export type { SlotsToClasses } from './types'
 export { colorVariants } from './variants'
 export { COMMON_UNITS, twMergeConfig } from './tw-merge-config'
 export { cn } from './cn'
